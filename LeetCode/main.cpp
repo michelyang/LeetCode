b996d88111b92cfe3040f01b97df1922b748d92e
michelyang <michelyang1990@gmail.com>
2017-01-24 15:55:04 -0500

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by Xuewen on 1/24/17.
+//  Copyright © 2017 Xuewen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
